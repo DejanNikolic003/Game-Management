9cb401de4987a5400f87d57d77035cc95489fe98
DejanNikolic003 <knolekjhnny@gmail.com>
2025-06-24 12:03:47 +0200

    Added routes for categories.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "node:path";
 import "dotenv/config";
+import { categoryRouter } from "./routes/categoryRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3030;
@@ -17,6 +18,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+
+app.use("/categories", categoryRouter);
 
 app.listen(PORT, () => {
     console.log(`Application is running on http://localhost:${PORT}`);
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import { createCategoryController } from "../controllers/CategoryController.js";
 
 export const categoryRouter = Router();
+
+categoryRouter.get("/create", (req, res) => res.render("categories/create"));
+categoryRouter.post("/create", createCategoryController);
