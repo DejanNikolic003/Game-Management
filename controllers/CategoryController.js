98b6dfa6539b7e6f3e634ae07fe8522b5a7852ab
DejanNikolic003 <knolekjhnny@gmail.com>
2025-06-24 12:15:35 +0200

    Added category deletion fn.

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,4 +1,4 @@
-import { createCategory } from "../models/Category.js";
+import { createCategory, deleteCategoryById, getAllCategories } from "../models/Category.js";
 
 
 export const createCategoryController = async (req, res) => {
@@ -6,5 +6,19 @@ export const createCategoryController = async (req, res) => {
 
     await createCategory(categoryName);
 
-    res.redirect("/categories/create");
+    res.redirect("/categories/");
 };
+
+export const getAllCategoriesController = async (req, res) => {
+    const { rows } = await getAllCategories();
+
+    res.render("categories/index", { categories: rows });
+};
+
+export const deleteCategoryController = async (req, res) => {
+    const { id } = req.params;
+
+    await deleteCategoryById(Number(id));
+
+    res.redirect("/categories/");
+}
--- models/Category.js
+++ models/Category.js
@@ -3,3 +3,13 @@ import { pool } from "../db/pool.js";
 export const createCategory = async (name) => {
     await pool.query("INSERT INTO categories (name, banner_url) VALUES ($1, $2)", [name, "nincs"]);
 };
+
+export const getAllCategories = async () => {
+    const rows = await pool.query("SELECT * FROM categories");
+
+    return rows;
+};  
+
+export const deleteCategoryById = async (id) => {
+    await pool.query("DELETE FROM categories WHERE id = ($1)", [id]);
+}
