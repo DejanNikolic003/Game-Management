8628e6bf30bd6d19cdcf304504a28c7005d1229a
DejanNikolic003 <knolekjhnny@gmail.com>
2025-06-24 11:30:28 +0200

    Basic project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "node:path";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+const __dirname = path.resolve();
+
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(PORT, () => {
+    console.log(`Application is running on http://localhost:${PORT}`);
+});
---
+++ db/pool.js
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+import "dotenv/config";
+
+export const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
