6fc0ffd84c2ee454574b654c72cfd5cddb89ea40
DejanNikolic003 <knolekjhnny@gmail.com>
2025-06-24 12:15:49 +0200

    Added index, and delete routes for categories.

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { createCategoryController } from "../controllers/CategoryController.js";
+import { createCategoryController, deleteCategoryController, getAllCategoriesController } from "../controllers/CategoryController.js";
 
 export const categoryRouter = Router();
 
+categoryRouter.get("/", getAllCategoriesController);
+
 categoryRouter.get("/create", (req, res) => res.render("categories/create"));
 categoryRouter.post("/create", createCategoryController);
+
+categoryRouter.post("/delete/:id", deleteCategoryController);
